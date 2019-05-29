87f34bba06f8d5e209cd4f861124af17e9420521
LY_Matser <yan51055105@163.com>
2019-05-29 21:50:54 +0800

    Added LCD function

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+	var str = arguments[0];
+	var arr = [["._.","...","._.","._.","...","._.","._.","._.","._.","._."],["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]];
+	var res = "";
+	for(i=0;i<3;i++){
+		var res1 = "";
+		for(j=0;j<str.length;j++){
+			var index = parseInt(str.charAt(j));
+			res1 = res1 + arr[i][index];
+		}	
+	res = res + res1 + "\n";
+	}
+	return res;
 };
